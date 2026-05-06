66efc130a773b8dfbbd91ea9f9ae41801f91b652
Hanuel08 <hanuel345martinez@gmail.com>
2026-05-05 23:32:06 -0400

    add Message component

---
+++ src/components/Message.jsx
@@ -0,0 +1,16 @@
+export function Message ({ msg, bgColor}) {
+  let styles = {
+    padding: "1rem",
+    marginBottom: "1rem",
+    textxAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    backgroundColor: bgColor
+  }
+
+  return (
+    <div style={styles}>
+      <p>{msg}</p>
+    </div>
+  )
+}
